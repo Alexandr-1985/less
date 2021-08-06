1dc1aece70ab82c1bd16c6ab942cb79df75b41bf
Alexandr Gorr <alexandrgorr@gmail.com>
2021-08-06 18:27:46 +0300

    commit lesson03

--- scripts/main.js
+++ scripts/main.js
@@ -2,68 +2,50 @@
 
 /////////////////////////////////////////////////////////////////////////
 //1 value assignment
-let money = 190000;
-let income = 'frilance';
-let addExpenses = 'Internet, Taxy, Communal payment, Credit';
-let deposit = true;
-let mission = 1000000;
-let period = 6;
-let budgetDay;
-let costs;
-let budgetMonth;
-let internet = 30000;
-let taxy = 15000;
-let communalPayment = 40000;
-let credit = 21000;
-
-//metods and properties
-
-console.log(typeof money, typeof income, typeof deposit);
-console.log(addExpenses.length);
-console.log(
-    'Period is equal ' + period + ' month.',
-    'Goal to earn ' + mission + ' euro!'
-);
-console.log(addExpenses.toLocaleLowerCase().split(' '));
-
-budgetDay = money /= 30;
-console.log(Math.round(budgetDay));
-//console.log(budgetDay.toFixed());
+let money,
+    income = 'Full Stack',
+    addExpenses,
+    deposit,
+    mission = 1000000,
+    period = 3,
+    budgetDay,
+    expenses1,
+    expenses2,
+    amount1,
+    amount2,
+    budgetMonth;
 
 /////////////////////////////////////////////////////////////////////////////////
 //question about salary
-money = prompt('Ваш месячный доход?', ' ');
-console.log(money);
+money = +prompt('Ваш месячный доход?', ' ');
+//console.log(money);
 
 addExpenses = prompt(
     'Перечислите возможные расходы за рассчитываемый период через запятую',
     'Internet, Taxy, Communal payment, Credit'
 );
-console.log(addExpenses);
-console.log(addExpenses.split(' '));
+//console.log(addExpenses);
+//console.log(addExpenses.split(' '));
 
-deposit = console.log(Boolean(prompt('Есть ли у вас депозит в банке')));
+deposit = confirm('Есть ли у вас депозит в банке?');
 
-costs = prompt('Есть ли у вас депозит в банке', 'expenses1');
-console.log(costs);
-costs = prompt('Есть ли у вас депозит в банке', 'expenses2');
-console.log(costs);
-
-costs = console.log(prompt('Во сколько это обойдется', 'amount1'));
-costs = console.log(prompt('Во сколько это обойдется', 'amount2'));
+expenses1 = prompt('Введите обязательную статью расходов?', ' ');
+amount1 = +prompt('Во сколько это обойдется?', ' ');
+expenses2 = prompt('Введите обязательную статью расходов?', ' ');
+amount2 = +prompt('Во сколько это обойдется?', ' ');
 
 //бюджет на месяц
-budgetMonth = money - internet - taxy - communalPayment - credit;
-console.log('Бюджет на месяц:', budgetMonth);
+budgetMonth = money - (amount1 + amount2);
+console.log('Бюджет на месяц: ', budgetMonth);
 
 //за сколько месяцев можно будет собрать mission
 mission = console.log(
-    'Цель бцдет достигнута за' + Math.round(mission / budgetMonth) + 'месяцев'
+    'Цель будет достигнута за ' + Math.round(mission / budgetMonth) + ' месяцев'
 );
 
 //budgetMonth учитывая бюджет на месяц
-budgetDay = budgetMonth /= 30;
-console.log('Бюджет на день:', Math.round(-budgetDay));
+budgetDay = budgetMonth / 30;
+console.log('Бюджет на день: ' + Math.floor(budgetDay) + ' рублей');
 
 //Конструктор условий
 if (budgetDay > 1200) {
@@ -84,23 +66,15 @@ if (budgetDay > 1200) {
     console.log('Иди к начальнику проси повышения');
 }
 
-//тернарный
+////////////////////////////////////////////////////////
+
+//metods and properties
+
+console.log(typeof money, typeof income, typeof deposit);
+console.log(addExpenses.length);
 console.log(
-    isNaN(budgetDay) ?
-    'Вероятно ошибка' :
-    budgetDay < 0 ?
-    'Что то пошло не так' :
-    budgetDay < 600 ?
-    'Надо стараться лучше!' :
-    budgetDay < 1200 ?
-    'Уже лучше' :
-    budgetDay > 1200 ?
-    'У вас высокий уровень дохода' :
-    budgetDay === 0 ?
-    'Меняй работу' :
-    budgetDay === 600 ?
-    'Надо стараться' :
-    budgetDay === 1200 ?
-    'You are good!' :
-    'Или просто не везет'
+    'Period is equal ' + period + ' month.',
+    'Goal to earn ' + mission + ' euro!'
 );
+console.log(addExpenses.toLocaleLowerCase().split(' '));
+console.log(Math.floor(budgetDay));
